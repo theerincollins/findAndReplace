00d4ba636ab232d46f5d5a2ccf303e1f2e146fde
Erin <erincollinsthethird@gmail.com>
2015-05-29 10:45:46 -0700

    will strip punctuation and replace with a new line

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 var findAndReplace = function(stringToCheck, checkWord, replacementWord) {
+  stringToCheck = takePunctuation(stringToCheck, checkWord, replacementWord)[1];
   return stringToCheck.replace(checkWord, replacementWord);
 };
 
-var puncuation = function(stringToCheck, checkWord, replacementWord) {
+var takePunctuation = function(stringToCheck, checkWord, replacementWord) {
+
   var regex = new RegExp (/[^0-9a-zA-Z\s]/g)
-  var puncuation = stringToCheck.match(regex)
-  return puncuation;
+  var punctuationArray = stringToCheck.match(regex)
+  punctuationArray.forEach(function(punctuationMark) {
+   stringToCheck = stringToCheck.replace(punctuationMark, "\n")
+  debugger;
+  })
+  return [punctuationArray, stringToCheck];
 };
--- spec/specs.js
+++ spec/specs.js
@@ -1,12 +1,20 @@
 describe('findAndReplace', function(){
   it("will replace a given word in a given string with another given word", function(){
-    expect(findAndReplace("is that a big dog?", "big", "small")).to.equal("is that a small dog?");
+    expect(findAndReplace("is that a big dog?", "big", "small")).to.equal("is that a small dog\n");
   });
 
 });
 
-describe('puncuation', function(){
-  it("will return the index and symbol of puncuation in a string", function(){
-    expect(puncuation("hello! there", "big", "dog")).to.eql(["!"])
+describe('takePunctuation', function(){
+  it("will return the symbol of puncuation in a string", function(){
+    expect(takePunctuation("hello! there", "big", "dog")).to.eql([["!"], "hello\n there"])
+  })
+
+  it("will return an array of symbols of puncuation in a string", function(){
+    expect(takePunctuation("hello! there:", "big", "dog")).to.eql([["!", ":"], "hello\n there\n"])
+  })
+
+  it("will replace the puncuation with new lines", function(){
+    expect(takePunctuation("hello! there:", "big", "dog")).to.eql([["!", ":"], "hello\n there\n"])
   })
 })
